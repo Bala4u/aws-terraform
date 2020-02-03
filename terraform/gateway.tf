29fdf5bc23a5afe386063b2563bbe6db53e22d4f
Anita Patil <anita.patil@sciex.com>
2020-02-03 19:26:13 +0530

    changed

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -4,14 +4,14 @@ resource "aws_api_gateway_rest_api" "hello_world_api" {
 }
 
 resource "aws_api_gateway_resource" "hello_world_api_gateway" {
-  rest_api_id = "aws_api_gateway_rest_api.hello_world_api.id"
+  rest_api_id = "aws_api_gateway_rest_api.hello_world_api"
   parent_id   = "aws_api_gateway_rest_api.hello_world_api.root_resource_id"
   path_part   = var.api_path
 }
 
 resource "aws_api_gateway_method" "hello_world_method" {
-  rest_api_id   = "aws_api_gateway_rest_api.hello_world_api.id"
-  resource_id   = "aws_api_gateway_resource.hello_world_api_gateway.id"
+  rest_api_id   = "aws_api_gateway_rest_api.hello_world_api"
+  resource_id   = "aws_api_gateway_resource.hello_world_api_gateway"
   http_method   = var.hello_world_http_method
   authorization = "NONE"
 }
