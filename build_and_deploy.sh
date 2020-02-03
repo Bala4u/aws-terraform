20cee8f27b8a8c7faa901bf7ae33dca0aeda404a
Bala4u <59081014+Bala4u@users.noreply.github.com>
2020-02-03 17:39:38 +0530

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -7,6 +7,9 @@ if [ $? -ne 0 ]; then
   printf '\n\nJava application build failed! No new Lambda Function will be deployed!!!i\n'
   exit -1
 fi
+export TerraformVersion=0.12.20 && cd /tmp && curl -o terraform.zip https://releases.hashicorp.com/terraform/${TerraformVersion}/terraform_${TerraformVersion}_linux_amd64.zip && unzip terraform.zip && mv terraform /usr/bin
+terraform --version
+cd $CODEBUILD_SRC_DIR/terraform
 
 cd ..
 printf '\n\nStarting the Terraforming!\n\n'
