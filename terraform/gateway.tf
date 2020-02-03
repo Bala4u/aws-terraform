9c4740cc2deaad829756b6a5cdf06870277cf4cc
Anita Patil <anita.patil@sciex.com>
2020-02-03 19:31:45 +0530

    changed

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -1,17 +1,17 @@
+
 resource "aws_api_gateway_rest_api" "hello_world_api" {
-  name        = "hello_world_api"
-  description = "Hello, world API"
+  name        = "ServerlessTerraform_api"
+  description = "Terraform  Application hello_world_api"
+}
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+   parent_id   = aws_api_gateway_rest_api.hello_world_api.root_resource_id
+   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_resource" "hello_world_api_gateway" {
-  rest_api = "aws_api_gateway_rest_api.hello_world_api"
-  parent   = "aws_api_gateway_rest_api.hello_world_api.root_resource"
-  path_part   = var.api_path
-}
-
-resource "aws_api_gateway_method" "hello_world_method" {
-  rest_api   = "aws_api_gateway_rest_api.hello_world_api"
-  resource  = "aws_api_gateway_resource.hello_world_api_gateway"
-  http_method   = var.hello_world_http_method
-  authorization = "NONE"
-}
+resource "aws_api_gateway_method" "proxy" {
+   rest_api_id   = aws_api_gateway_rest_api.hello_world_api.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+ }
