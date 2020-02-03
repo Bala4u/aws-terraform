13a43f277b626e6ab74e313c16399393046522ad
Anita Patil <anita.patil@sciex.com>
2020-02-03 19:53:18 +0530

    changed

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "hello_world_function" {
-  function_name = "hello_function"
   filename      = var.lambda_payload_filename
+  function_name = "hello_function"
 
   role             = "aws_iam_role.lambda_apigateway_iam_role.arn"
   handler          = var.lambda_function_handler
-  #source_code_hash = "base64sha256(file(var.lambda_payload_filename))"
+  source_code_hash = "base64sha256(file(var.lambda_payload_filename))"
   runtime          = "java8"
 
 }
